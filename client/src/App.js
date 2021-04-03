13d1817a9223475388aa33d65a2064970fc56ffb
Shamma Qureshi <shammaqureshi@shammas-mbp.lan>
2021-04-03 14:36:08 -0400

    Changed login routes

--- client/src/App.js
+++ client/src/App.js
@@ -49,13 +49,13 @@ function App() {
   const handleLogin = async (formData) => {
     const userData = await loginUser(formData);
     setCurrentUser(userData);
-    history.push("/");
+    history.push("/cars");
   };
 
   const handleRegister = async (formData) => {
     const userData = await registerUser(formData);
     setCurrentUser(userData);
-    history.push("/");
+    history.push("/cars");
   };
 
   const handleLogout = () => {
