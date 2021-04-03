c7bda051b046dd50547665d7f3c52b696f4c1080
Shamma Qureshi <shammaqureshi@Shammas-MacBook-Pro.local>
2021-04-03 19:19:29 -0400

    All Cars Screen 2.0

--- client/src/screens/AllCars/AllCars.jsx
+++ client/src/screens/AllCars/AllCars.jsx
@@ -5,22 +5,26 @@ import "../AllCars/AllCars.css";
 export default function AllCars(props) {
   const { cars } = props;
   return (
-    <div>
-      <h3>All Cars</h3>
+    <div className="all-page">
+      <div className="all-heading">Shop Our Inventory</div>
       <div className="all-car-container">
         {cars.map((cars) => (
           <React.Fragment key={cars.id}>
-            <Link to={`/cars/${cars.id}`}>
+            <Link to={`/cars/${cars.id}`} className="all-link">
               <div className="each-car">
                 <img
                   className="all-car-image"
                   src={cars.img_url}
                   alt={cars.make}
                 />
-                <div className="all-year">{cars.year}</div>
-                <div className="all-make">{cars.make}</div>
-                <div className="all-model">{cars.model}</div>
-                <div className="all-price">${cars.price}</div>
+                <div className="all-details">
+                  <div className="all-make-model">
+                    {cars.make} {cars.model}
+                  </div>
+                  <div className="all-year">{cars.year}</div>
+                  <div className="all-price">${cars.price}</div>
+                </div>
+                <button className="all-button">Learn More</button>
               </div>
             </Link>
           </React.Fragment>
