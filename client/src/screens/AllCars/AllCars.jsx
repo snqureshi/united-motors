9c1ae917704a19ed15ec6490155a3359d693f500
Shamma Qureshi <shammaqureshi@Shammas-MacBook-Pro.local>
2021-04-04 19:45:20 -0400

    Styled Search Bar

--- client/src/screens/AllCars/AllCars.jsx
+++ client/src/screens/AllCars/AllCars.jsx
@@ -1,13 +1,26 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "../AllCars/AllCars.css";
+import Search from "../../components/Search";
 
 export default function AllCars(props) {
+  const [queriedCars, setQueriedCars] = useState([]);
   const { cars } = props;
+
+  const handleSearch = (e) => {
+    const filteredCars = cars.filter((car) =>
+      car.make.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+    setQueriedCars(filteredCars);
+  };
+
+  const handleSubmit = (event) => event.preventDefault();
 
   return (
     <div className="all-page">
       <div className="all-heading">Shop Our Inventory</div>
+      <Search onSubmit={handleSubmit} onChange={handleSearch} />
       <div className="all-car-container">
         {cars.map((cars) => (
           <React.Fragment key={cars.id}>
