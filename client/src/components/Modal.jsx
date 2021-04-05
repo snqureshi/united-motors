3394cdeb02ac66f7eddf12a18cc2d87772203f79
Shamma Qureshi <shammaqureshi@Shammas-MacBook-Pro.local>
2021-04-05 17:41:38 -0400

    Modal component styled

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Modal.css";
 
 export default function Modal(props) {
   const { open, handleOpen, handleDelete } = props;
@@ -6,18 +7,20 @@ export default function Modal(props) {
     <div className="modal-container" onClick={(e) => handleOpen(false)}>
       <div className="modal" onClick={(e) => e.stopPropagation()}>
         <p>Are you sure? This action cannot be reversed</p>
-        <button className="modal-button" onClick={() => handleOpen(false)}>
-          no
-        </button>
-        <button
-          className="modal-button"
-          onClick={() => {
-            handleDelete(open);
-            handleOpen(false);
-          }}
-        >
-          yes
-        </button>
+        <div className="modal-button-container">
+          <button className="modal-button" onClick={() => handleOpen(false)}>
+            no
+          </button>
+          <button
+            className="modal-button"
+            onClick={() => {
+              handleDelete(open);
+              handleOpen(false);
+            }}
+          >
+            yes
+          </button>
+        </div>
       </div>
     </div>
   );
