5b2a5500b908aaad5a79842f0b7e4741cd8beb13
Shamma Qureshi <shammaqureshi@Shammas-MacBook-Pro.local>
2021-04-05 21:51:18 -0400

    Fixed Map Container

--- client/src/containers/MapContainer.jsx
+++ client/src/containers/MapContainer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 const MapContainer = () => {
   const mapStyles = {
@@ -10,13 +10,24 @@ const MapContainer = () => {
     lat: 40.7664872304385,
     lng: -73.3085163526681,
   };
+
+  const locations = [
+    {
+      name: "Location 1",
+      location: {
+        lat: 41.3954,
+        lng: 2.162,
+      },
+    },
+  ];
+
   return (
     <LoadScript googleMapsApiKey="AIzaSyChQY4homTSW3vo4qhYHCxRLtkfCb1hOTQ">
-      <GoogleMap
-        mapContainerStyle={mapStyles}
-        zoom={13}
-        center={defaultCenter}
-      />
+      <GoogleMap mapContainerStyle={mapStyles} zoom={13} center={defaultCenter}>
+        {locations.map((item) => {
+          return <Marker key={item.name} position={item.location} />;
+        })}
+      </GoogleMap>
     </LoadScript>
   );
 };
